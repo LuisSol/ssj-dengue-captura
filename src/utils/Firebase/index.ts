4cbc682ee74da48d13359286ee0c200f376e53ba
Luis Enrique Solis Navarro <luis.solis.ext@geodis.com>
2021-01-28 21:12:38 -0600

    fix db

--- src/utils/Firebase/index.ts
+++ src/utils/Firebase/index.ts
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOM,
-  projectId: process.env.REACT_APP_FIREBASE_PRO_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STOR_BUC,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MES_ID,
+  apiKey: "AIzaSyBiwKV78u9o4RL-5K3Tn-hap7mgwCFHf8U",
+  authDomain: "captura-dengue-ssj.firebaseapp.com",
+  projectId: "captura-dengue-ssj",
+  storageBucket: "captura-dengue-ssj.appspot.com",
+  messagingSenderId: "135157143183",
+  appId: "1:135157143183:web:6a468f635400c168d1c2fa",
+  measurementId: "G-W8WQED8LV6",
 };
 
 if (!firebase.apps.length) {
